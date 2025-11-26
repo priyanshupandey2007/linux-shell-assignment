93ae13a2bdebae2a710a20ff9d49425102b85b35
priyanshupandey2007 <priyanshupandey2864@gmail.com>
2025-11-26 14:44:06 +0530

    Update download.sh

--- linux assignment/download.sh
+++ linux assignment/download.sh
@@ -1,1 +1,18 @@
+#Script Name: download.sh
+#Purpose: To download a file from the internet
+#Author: Priyanshu pandey
+#Date: 17-11-2025
 
+#File link to download
+URL= https://example-files.online-convert.com/document/txt/example.txt
+
+#Folder where file will be saved
+SAVE_DIR= /home/priyanshu/Downloads/ScriptDownloads
+
+#Create folder if not exists
+mkdir -p $SAVE_DIR
+
+#Download file
+wget -p $SAVE_DIR $URL
+
+echo File downloaded into $SAVE_DIR
