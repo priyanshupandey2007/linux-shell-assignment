281ab6f87a665029cf6dc442a3edcb14ad3d365a
priyanshupandey2007 <priyanshupandey2864@gmail.com>
2025-11-26 14:42:56 +0530

    Update monitor.sh

--- linux assignment/monitor.sh
+++ linux assignment/monitor.sh
@@ -1,1 +1,21 @@
+#Script Name: monitor.sh
+#Purpose: To log CPU and Memory usage every 5 seconds
+#Author: Priyanshu pandey
+#Date: 17-11-2025
 
+LOGFILE=/home/priyanshu/system_log.txt
+
+echo Monitoring started... Logging into $LOGFILE
+
+while true
+do
+    echo ------ $(date) ------ >> $LOGFILE
+    echo CPU Usage: >> $LOGFILE
+    top -bn1 grep Cpu(s) >> $LOGFILE
+
+    echo Memory Usage: >> $LOGFILE
+    free -h >> $LOGFILE
+
+    echo ---------------------------- >> $LOGFILE
+    sleep 5
+done
